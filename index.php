2de782bb82e04815a6e9683e3c3193383da50c60
Tkacz <gabrielmt2@al.insper.edu.br>
2022-07-08 02:10:29 -0300

    att

--- index.php
+++ index.php
@@ -14,33 +14,46 @@ $allPlayers = Players::getPlayers();
 $results = "";
 
 foreach ($allPlayers as $row) {
+    $end_date_init = $row->BID_START_DATE;
+    $end_date_raw = date('Y-m-d H:i:s', strtotime($end_date_init. ' + 1 days'));
+    $end_date = str_replace(" ", "T", $end_date_raw);
+    $date_obj = new DateTime($end_date_raw);
+    $now = new DateTime();
+    
     if($row->PLAYER_TYPE != 'SIGNED' && $row->BID_VALUE != ''){
-        $end_date_raw = $row->BID_START_DATE;
-        $end_date_raw = date('Y-m-d H:i:s', strtotime($end_date_raw. ' + 1 days'));
-        $end_date = str_replace(" ", "T", $end_date_raw);
-        
-        $results .= '<div class="div-leilao">
-        <div class="card hover-overlay hover-zoom hover-shadow ripple">
-            <a href="resources/views/edit-bid?ID=' . $row->getSlug("NBA_ID") . '">
-            <span class="edit-content">+</span>
-            </a>
-            <b>' . $row->NAME . '</b>
-            <span>' . $row->getPosition() . '</span>
-            <em>$ ' . number_format($row->BID_VALUE) . '</em>
-            <em>' . $row->BID_YEARS . ' anos</em>
-            <em> Maior bid: ' . $row->BID_WINNER . '</em>
-        </div>
-        <script language="JavaScript">
-            TargetDate = "' . $end_date . '";
-            ForeColor = "black";
-            CountActive = true;
-            CountStepper = -1;
-            LeadingZero = true;
-            DisplayFormat = "%%H%% h %%M%% min %%S%% s";
-            FinishMessage = "Leilão encerrado.";
-        </script>
-        <script language="JavaScript" src="https://rhashemian.github.io/js/countdown.js"></script>
-        </div>';
+        if($date_obj > $now) {
+            $results .= '<div class="div-leilao">
+            <div class="card hover-overlay hover-zoom hover-shadow ripple">
+                <a href="resources/views/edit-bid?ID=' . $row->getSlug("NBA_ID") . '">
+                <span title="Bidar esse jogador" class="edit-content">+</span>
+                </a>
+                <b>' . $row->NAME . '</b>
+                <span>' . $row->getPosition() . '</span>
+                <em>$ ' . number_format($row->BID_VALUE) . '</em>
+                <em>' . $row->BID_YEARS . ' ano(s)</em>
+                <em> Maior bid: ' . $row->BID_WINNER . '</em>
+            </div>
+            <p id="demo"></p>
+            <script>
+                var countDownDate = new Date("' . $end_date . '").getTime();
+                var x = setInterval(function() {
+                var now = new Date().getTime();
+                var distance = countDownDate - now;
+                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                document.getElementById("demo").innerHTML = + hours + "h "
+                + minutes + "min " + seconds + "s";
+                if (distance < 0) {
+                  clearInterval(x);
+                  document.getElementById("demo").innerHTML = "Leilão encerrado.";
+                }
+              }, 1000);
+              </script>
+            </script>
+            </div>';
+        }
     }
 }
 ?>
--- resources/views/partials/head.php
+++ resources/views/partials/head.php
@@ -12,6 +12,8 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
 <script type="text/javascript" src="resources/js/main.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/timecircles/1.5.3/TimeCircles.min.js" integrity="sha512-FofOhk0jW4BYQ6CFM9iJutqL2qLk6hjZ9YrS2/OnkqkD5V4HFnhTNIFSAhzP3x//AD5OzVMO8dayImv06fq0jA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Fantasy Trash Talk</title>
