3a5f4d7e3a958f6406ddf3cd200fcde7f8d0e318
Tkacz <gabrielmt2@al.insper.edu.br>
2022-07-08 05:26:22 -0300

    att

--- resources/views/admin.php
+++ resources/views/admin.php
@@ -2,33 +2,63 @@
 
 require_once("../../vendor/autoload.php");
 
+use App\models\Players;
+use App\models\User;
+
 include("partials/head.php");
+
+if (!isset($_SESSION["username"])){
+    header('location: log-in?index=true');
+}
+
+if ($_SESSION["commissioner"] != 1){
+    header('location: index?admin=true');
+}
+
+$allUsers = User::getUsers();
+$allPlayers = Players::getPlayers();
+
+$resultsUsers = "";
+$resultsPlayers = "";
+
+foreach ($allUsers as $row) {
+    $resultsUsers .= '<tr>
+                <td class="texto-bandeira"><a href="/edit?" class="a"><img src="resources/images/edit_black_24dp.svg" class="logo"/>' . $row->USERNAME . '</td>
+                <td>' . $row->TEAMNAME . '</td>
+                <td>' . number_format($row->CAP) . '</td>
+                <td>' . number_format($row->SLOTS) . '</td>
+                <td>' . number_format($row->IS_COMMISSIONER) . '</td>
+            </tr>';
+}
+
 ?>
 <body>
 <div class="container">
-    <form method="post" action="sign-up.inc" autocomplete="off" id="signup_form">
-        <div class="title">
-            <h2>Criar uma conta</h2>
-            <div>
-                <button type="submit" class="btn btn-success btn-size">Criar</button>
-                <button type="button" class="btn btn-danger btn-size" onclick="window.location.href='index'">Cancelar</button>
-            </div>
+    <div class="title">
+        <h2>Painel de administração</h2>
+        <div>
+            <button type="submit" class="btn btn-primary btn-size" onclick="window.location.href='admin'">Usuários</button>
+            <button type="button" class="btn btn-primary btn-size" onclick="window.location.href='index'">Jogadores</button>
         </div>
+    </div>
 
-        <hr>
+    <hr>
 
-        <div class="form">
-            <div class="form-item">
-                <label for="username">Nome de usuário</label>
-                <input type="text" maxlength="30" id="username" name="username" placeholder="Escolha seu nome de usuário" oninvalid="this.setCustomValidity('Por favor escolha seu nome de usuário.')" oninput="this.setCustomValidity('')" required><br>
-                
-                <label for="password">Senha</label>
-                <input type="password" maxlength="30" id="password" name="password" placeholder="Escolha sua senha" oninvalid="this.setCustomValidity('Por favor escolha sua senha.')" oninput="this.setCustomValidity('')" required><br>
-
-                <label for="teamname">Nome do time</label>
-                <input type="text" maxlength="30" id="teamname" name="teamname" placeholder="Escolha o nome do seu time" oninvalid="this.setCustomValidity('Por favor escolha o nome do seu time.')" oninput="this.setCustomValidity('')" required><br>
-            </div>
-        </div>
-    </form>
+    <center>
+    <table class="styled-table">
+        <thead>
+        <tr>
+            <th><span class="a-titulo">Nome de usuário</span></th>
+            <th><span class="a-titulo">Nome do time</span></th>
+            <th><span class="a-titulo">Cap disponível</span></th>
+            <th><span class="a-titulo">Slots disponíveis</span></th>
+            <th><span class="a-titulo">É comissário</span></th>
+        </tr>
+        </thead>
+        <tbody>
+        <?= $resultsUsers ?>
+        </tbody>
+        </table>
+    </center>
 </div>
 <?php include("partials/footer.php"); ?>
