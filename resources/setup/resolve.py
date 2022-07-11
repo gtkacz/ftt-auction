e7c59231c3605beb45667bd1fbc7b25d4a7d154e
Tkacz <gabrielmt2@al.insper.edu.br>
2022-07-10 21:15:59 -0300

    att

---
+++ resources/setup/resolve.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def main():
+    df = pd.read_csv('players.csv')
+    df.drop(['ID', 'POSITION1', 'POSITION2', 'PLAYER_TYPE', 'BID_VALUE', 'BID_YEARS', 'HAS_TO', 'BID_START_DATE', 'BID_WINNER',], axis=1, inplace=True)
+    id_list = []
+    
+    for index, row in df.iterrows():
+        resultado = input(f'{row["NAME"]}: ')
+        if resultado == 'y':
+            id_list.append(str(row['NBA_ID']))
+            
+        if index == 2:
+            break
+        
+    ids = ', '.join(id_list)
+    ids = f'({ids})'
+    query = f'UPDATE players SET PLAYER_TYPE = "SIGNED" WHERE NBA_ID IN {ids}'
+    with open("query.txt", "w") as file:
+        file.write(query)
+
+if __name__ == '__main__':
+    main()
